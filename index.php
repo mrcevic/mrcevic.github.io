f7484f202fb5a80a9e867e002aca7b9338067081
VHC <moncho@gmail.com>
2021-07-01 15:13:01 +0100

    Update index.php
    Added shorthand echo code

--- index.php
+++ index.php
@@ -3,25 +3,18 @@ $iframe_option = "SAMEORIGIN";
 header ("X-Frame-Options: $iframe_option");
 ?>
 
-
-
 <!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <title>A Basic Template</title>
   <meta name="description" content="A basic HTML5 Template.">
-
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
-
 </head>
 
 <body>
-
 <img src="lulioKKI.png" alt="KKI in Lulio" width="250" />
-<p><b>iFrame option selected:</b> <?php echo $iframe_option; ?></p>
+<p><b>iFrame option selected:</b> <?=$iframe_option?></p>
 
 <button type="button" onclick="window.history.back();">JS Go Back</button>
 
